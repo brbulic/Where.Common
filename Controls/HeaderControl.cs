1c942e726fe504f95c11b32e4f21a732bc2825e0
Bruno Bulic <bbulic@paypal.com>
2011-09-20 15:24:54 +0200

    Removed unused Imports in HeaderControl.cs

--- Controls/HeaderControl.cs
+++ Controls/HeaderControl.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Where.Controls
 {
