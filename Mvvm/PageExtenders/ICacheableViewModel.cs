48f58bbef8fa7eb1a68ddb57a0896df21651845f
Bruno Bulic <bbulic@paypal.com>
2011-09-21 14:09:11 +0200

    Added PageExtenders interfaces for extending PageViewModel support

---
+++ Mvvm/PageExtenders/ICacheableViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Where.Common.Services.Interfaces;
+
+namespace Where.Common.Mvvm.PageExtenders
+{
+	interface ICacheableViewModel<T> where T : IWhereCacheable
+	{
+		T LoadFromCache();
+
+		void SaveToCache(T save);
+	}
+}
