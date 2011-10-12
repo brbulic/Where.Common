7bbf04f56809fa62b336c73f369298fcf93d0061
Bruno Bulic <bbulic@paypal.com>
2011-10-12 17:19:19 +0200

    - Added IListMenuItem interface for favoritism

---
+++ Controls/IListMenuItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Where.Common.Controls
+{
+	public interface IListMenuItem : INotifyPropertyChanged
+	{
+		ICommand MenuItemClick { get; }
+		string CommandTitle { get; }
+	}
+}
